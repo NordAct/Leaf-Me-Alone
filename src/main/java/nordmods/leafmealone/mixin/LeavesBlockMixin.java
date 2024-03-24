1af12a070380595010720a79d571f7bb5206e238
NordAct <73118477+NordAct@users.noreply.github.com>
2024-03-24 16:41:34 +0300

    Make leaves have no collision on client if entity is on vehicle

--- src/main/java/nordmods/leafmealone/mixin/LeavesBlockMixin.java
+++ src/main/java/nordmods/leafmealone/mixin/LeavesBlockMixin.java
@@ -1,6 +1,7 @@
 package nordmods.leafmealone.mixin;
 
 import net.minecraft.block.*;
+import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.shape.VoxelShape;
 import net.minecraft.util.shape.VoxelShapes;
@@ -13,8 +14,14 @@ public abstract class LeavesBlockMixin extends Block {
         super(settings);
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public VoxelShape getCollisionShape(BlockState state, BlockView world, BlockPos pos, ShapeContext context) {
+        if (world instanceof ClientWorld) {
+            if (context instanceof EntityShapeContext entityShapeContext)
+                if (entityShapeContext.getEntity() != null && entityShapeContext.getEntity().getControllingVehicle() != null) return VoxelShapes.empty();
+        }
+
         if (context instanceof EntityShapeContext entityShapeContext)
             if (entityShapeContext.getEntity() != null && entityShapeContext.getEntity().hasControllingPassenger()) return VoxelShapes.empty();
         return super.getCollisionShape(state, world, pos, context);
